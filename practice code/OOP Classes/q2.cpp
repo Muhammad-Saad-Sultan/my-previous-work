d0048c88f0b1e2c964cf9903f2ece408092c7998
Muhammad-Saad-Sultan <saadsultan00000021@gmail.com>
2023-10-09 12:25:39 +0500

    Access Specifier edit

--- practice code/OOP Classes/q2.cpp
+++ practice code/OOP Classes/q2.cpp
@@ -3,13 +3,28 @@ using namespace std;
 
 class students
 {
-	private:
+	public:
+		int a;
+		int b;
+		int c;
 		
+	private:
+		int x;		
+		int y;		
+		int z;		
+		
+};
 
-}
 int main()
 {
+	students myobj;
+	myobj.a = 10;
+	myobj.b = 20;
+	myobj.c = 30;
 
+	cout << myobj.a << endl;
+	cout << myobj.b << endl;
+	cout << myobj.c << endl;
 
 return 0;
 }
